4b09789b4d99a0e1e0126daa660be3391e296c82
Maxim Lee <ml.js.dev@gmail.com>
2020-11-18 00:09:35 +0300

    update v.2.01 getBalance adjustments

--- test/winningScenario.test.js
+++ test/winningScenario.test.js
@@ -5,12 +5,14 @@ describe("Winning Scenario", () => {
     test('Session Initialization', async () => {
         const response = await functions.sessionInit(variables.sessionInitBody, variables.header);
         const { status, data, config } = response;
-        //expect(data.licenseeSessionId).toEqual(variables.sessionInitBody.licenseeSessionId);
         expect(status).toEqual(200);
+        expect(data.licenseePlayerId).toEqual(variables.sessionInitBody.licenseePlayerId);
+        expect(data.licenseeSessionId).not.toBeNull();
+        expect(data.currency).toEqual(variables.sessionInitBody.currency);
         
         console.log('Request Data', config.data);
         console.log('Response Data', data);
-        console.log(`Status: ${response.status} ${response.statusText}`);
+        console.log(`Status: ${response.status} ${response.statusText} & Player ID: ${response.data.licenseePlayerId}`);
     });
 
     test('Finish Initialization', async () => {
@@ -18,17 +20,42 @@ describe("Winning Scenario", () => {
         const { status, data, config } = response;
         expect(status).toEqual(201);
         console.log('Request Data', config.data);
-        console.log('Response Data');
         console.log(`Status: ${response.status} ${response.statusText}`);
     });
 
     test('Get Balance', async () => {
-        console.log("balance request", variables.balanceBody.sessionId);
         const response = await functions.getBalance(variables.balanceBody, variables.header);
         const {status, data, config} = response;
+        const currentBalance = data.balances[0].amount;
+        expect(currentBalance).not.toBeNull();
+        expect(status).toEqual(200);
+        expect(data.balances);
+        expect(status).not.toEqual('error');
         console.log('Request Data', config.data);
         console.log('Response Data', data);
         console.log(`Status: ${response.status} ${response.statusText}`)
+        console.log(`Player balance = ${currentBalance} ${variables.sessionInitBody.currency}`);
+    });
+
+    test('Get Balance for Table', async () => {
+        const response = await functions.getTableBalance(variables.balanceTableBody, variables.header);
+        const {status, data, config} = response;
+        const currentBalance = data.balances[0].amount;
+        expect(currentBalance).not.toBeNull();
         expect(status).toEqual(200);
+        expect(data.balances);
+        console.log('Request Data', config.data);
+        console.log('Response Data', data);
+        console.log(`Status: ${response.status} ${response.statusText}`)
+        console.log(`Table balance = ${currentBalance} ${variables.sessionInitBody.currency}`);
+    });
+    
+    test('Placebet Request', async () => {
+        const response = await functions.placeBetFirst(variables.placeBet1Body, variables.header);
+        const {status, data, config} = response;
+        expect(status).toEqual(200);
+        console.log('Request Data', config.data);
+        console.log('Response Data', data);
+        console.log(`Initial Balance = ${data.balances[0].amount}`);
     });
 });
